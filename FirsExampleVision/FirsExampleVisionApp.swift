80c8f6b6df8c5007f00157d90f6e6ee14853b539
AndresAleu <andresaleunz@gmail.com>
2023-06-27 12:49:55 +0200

    Initial Commit

---
+++ FirsExampleVision/FirsExampleVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirsExampleVisionApp.swift
+//  FirsExampleVision
+//
+//  Created by Andres Aleu on 27/6/23.
+//
+
+import SwiftUI
+
+@main
+struct FirsExampleVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
